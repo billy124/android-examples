32044b793454e25d3774e787b2dead358a7209e0
billy124 <billy124@msn.com>
2015-01-12 21:00:33 +0000

    working on speak button

--- androidExamples/app/build/gen/com/mycompany/myapp/R.java
+++ androidExamples/app/build/gen/com/mycompany/myapp/R.java
@@ -15,7 +15,8 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int location=0x7f060001;
+        public static final int btnSpeak=0x7f060001;
+        public static final int location=0x7f060002;
         public static final int search=0x7f060000;
     }
     public static final class layout {
--- androidExamples/app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ androidExamples/app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -2,13 +2,73 @@ package com.mycompany.myapp;
 
 import android.app.*;
 import android.os.*;
+import android.widget.*;
+import android.content.pm.*;
+import android.content.*;
+import android.speech.*;
+import java.util.List;
+import android.view.View.*;
+import android.view.*;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends Activity 
 {
+	private static final int REQUEST_CODE = 1234;
+    private ListView wordsList;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		Button btnSpeak = (Button) findViewById(R.id.btnSpeak);
+		
+		PackageManager pm = getPackageManager();
+		List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+		
+		if(activities.size() == 0)
+		{
+			btnSpeak.setEnabled(false);
+			btnSpeak.setText("Recognizer not present");
+		}
+		
+		
+		btnSpeak.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v)
+				{
+					// TODO: Implement this method
+					Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+					intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+									RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+					intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice recognition Demo...");
+					startActivityForResult(intent, REQUEST_CODE);
+				
+				}
+			});
+		
+			
     }
+	
+	/**
+     * Handle the results from the voice recognition activity.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK)
+        {
+            // Populate the wordsList with the String values the recognition engine thought it heard
+            ArrayList<String> matches = data.getStringArrayListExtra(
+				RecognizerIntent.EXTRA_RESULTS);
+            wordsList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+														  matches));
+        }
+        
+		super.onActivityResult(requestCode, resultCode, data);
+    }
+	
 }
