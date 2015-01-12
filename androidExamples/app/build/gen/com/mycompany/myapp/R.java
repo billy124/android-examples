75c0b8fb9771d54d2f11f80758a5cfc41113857d
billy124 <billy124@msn.com>
2015-01-12 22:04:58 +0000

    fixing commit issues between phone and laptop

--- androidExamples/app/build/gen/com/mycompany/myapp/R.java
+++ androidExamples/app/build/gen/com/mycompany/myapp/R.java
@@ -15,9 +15,11 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int btnSpeak=0x7f060001;
-        public static final int location=0x7f060002;
-        public static final int search=0x7f060000;
+        public static final int EditText01=0x7f060000;
+        public static final int btnLocation=0x7f060004;
+        public static final int btnSpeak=0x7f060002;
+        public static final int location=0x7f060003;
+        public static final int search=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
