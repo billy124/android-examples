704549250be1c2c9b8f0182d594dae77d7b7de9e
Billy Mahmood <billy124@msn.com>
2015-01-12 21:12:55 +0000

    fixed the speach listner

--- androidExamples/app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ androidExamples/app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -40,35 +40,30 @@ public class MainActivity extends Activity
 				@Override
 				public void onClick(View v)
 				{
-					// TODO: Implement this method
-					Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-					intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-									RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-					intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice recognition Demo...");
-					startActivityForResult(intent, REQUEST_CODE);
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    // Start the activity, the intent will be populated with the speech text
+                    startActivityForResult(intent, REQUEST_CODE);
 				
 				}
 			});
 		
 			
     }
-	
-	/**
-     * Handle the results from the voice recognition activity.
-     */
+
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK)
-        {
-            // Populate the wordsList with the String values the recognition engine thought it heard
-            ArrayList<String> matches = data.getStringArrayListExtra(
-				RecognizerIntent.EXTRA_RESULTS);
-            wordsList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-														  matches));
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            // Do something with spokenText
         }
-        
-		super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 	
 }
